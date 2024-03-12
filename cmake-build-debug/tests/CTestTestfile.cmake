a845c2aa4758cfc9860d5b588259b081ba058ad5
Лимонников Алексей <allimon472@gmail.com>
2024-03-12 14:14:23 +0400

    sec2

---
+++ cmake-build-debug/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Lexus/Desktop/LR1-main/tests
+# Build directory: C:/Users/Lexus/Desktop/LR1-main/cmake-build-debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/Lexus/Desktop/LR1-main/cmake-build-debug/tests/lr1_tests[1]_include.cmake")
+subdirs("../_deps/googletest-build")
